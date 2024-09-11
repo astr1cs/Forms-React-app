494ba3bcbe444d2469064bf5b61bc6bd450bfb0f
Astr1cs <merazuddin003@gmail.com>
2024-09-11 21:55:52 +0600

    Expense tracker

--- src/Componenets/Forms.tsx
+++ src/Componenets/Forms.tsx
@@ -1,9 +1,19 @@
 import { FormEvent, useRef, useState } from "react";
 import {useForm} from "react-hook-form";
+import {z} from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const schema=z.object({
+  description:z.string().min(3, {message:'Name must be atleast 3 characters.'}),
+  category:z.string().min(3, {message:'Category Field is required.'}),
+  amount:z.number( {invalid_type_error:'Age Filed is required.'}).min(0.01,{message: 'Age must be atleast 18.'})
+})
+
+type FormData =z.infer<typeof schema>;
 
 const Forms = () => {
 
-    const {register,handleSubmit}=useForm();
+    const {register,handleSubmit, formState:{errors,isValid} }=useForm<FormData>({resolver:zodResolver(schema)});
 
 const onSubmit =(data:any) => {console.log(data);  }
 
@@ -11,19 +21,37 @@ const onSubmit =(data:any) => {console.log(data);  }
     <>
     <form onSubmit={handleSubmit(onSubmit)} >
     <div className="mb-3">
-        <label htmlFor="name" className="form-label">Name:</label>
-            <input {...register('name')}
+
+        <label htmlFor="description" className="form-label">Description</label>
+
+            <input {...register('description',{required:true, minLength:3})}
               id="name"
             type="text" 
             className="form-control" 
             style={{width:"200px"}} />
+            {errors.description && <p className="text-danger">{errors.description.message}</p>}
             </div>
+
              <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age:</label>
-            <input  id="age" {...register("age")}
+
+        <label htmlFor="amount" className="form-label">Amount</label>
+
+            <input  id="amount" {...register("amount", {valueAsNumber:true})}
              type="number" className="form-control" style={{width:"200px"}}/>
+
+        <label htmlFor="category" className="form-label">Category</label>
+
+            <input  id="category" {...register("category", {required:true})}
+             type="text" className="form-control" style={{width:"200px"}}/>
+
+              {errors.category && <p className="text-danger">{errors.category.message}</p>}
             </div>
-            <button type='submit'  className='btn btn-primary'>Submit</button>
+
+
+
+
+
+            <button disabled={!isValid} type='submit'  className='btn btn-primary'>Submit</button>
             </form>
     </>
   )
